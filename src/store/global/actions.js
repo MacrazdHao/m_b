52b7f49c95905d182d4f40699769ed7791a4d138
MacrazdS <695257347@qq.com>
2021-07-30 18:26:42 +0800

    切换动态路由

--- src/store/global/actions.js
+++ src/store/global/actions.js
@@ -14,141 +14,141 @@ export default {
   getMenu: ({ commit, state }, data) => {
     return new Promise(resolve => {
       console.log('获取菜单路由')
-      let res = [{
-        path: '/index',
-        children: [
-          {
-            path: '/dashboard',
-            children: [{
-              path: '/schoolDashboard'
-            }, {
-              path: '/adminDashboard'
-            }, {
-              path: '/studentDashboard'
-            }, {
-              path: '/schedules'
-            }]
-          }, {
-            path: '/counseling',
-            children: [{
-              path: '/baseInfo',
-            }, {
-              path: '/onlineTest',
-            }, {
-              path: '/consultLive',
-              meta: { step: 4 }
-            }, {
-              path: '/getReport',
-            }]
-          }, {
-            path: '/knowledge'
-          }, {
-            path: '/students',
-            children: [{
-              path: '/studentsList'
-            }, {
-              path: '/studentsDetail'
-            }, {
-              path: '/studentsProcess'
-            }]
-          }, {
-            path: '/archives',
-            children: [{
-              path: '/archivesList'
-            }, {
-              path: '/archivesDetail'
-            }]
-          }, {
-            path: '/messages'
-          }, {
-            path: "/personal",
-            children: [{
-              path: "/studentBase",
-            }, {
-              path: "/schoolBase",
-            }, {
-              path: "/consultantBase",
-            }, {
-              path: "/adminBase",
-            }, {
-              path: "/account",
-            }, {
-              path: "/safe",
-            }]
-          }, {
-            path: "/accounts",
-            children: [{
-              path: "/admin",
-            }, {
-              path: "/schoolaccount",
-            }, {
-              path: "/teacher",
-            }, {
-              path: "/accountlist",
-            }, {
-              path: "/adminaccountlist",
-            }, {
-              path: "/schoolaccountlist",
-            }, {
-              path: "/invitation",
-            }]
-          }, {
-            path: '/live'
-          }, {
-            path: '/files',
-            children: [{
-              path: '/filesList'
-            }, {
-              path: '/filesDetail'
-            }]
-          },
-          {
-            path: "/management",
-            children: [{
-              path: "/counselinglist",
-            }, {
-              path: "/counselingDetail",
-            }, {
-              path: "/careerlist",
-            }, {
-              path: "/careerDetail",
-            }, {
-              path: "/courselist",
-            }, {
-              path: "/adviselist",
-            }]
-          },
-          {
-            path: "/school",
-            children: [{
-              path: "/schoollist",
-            }, {
-              path: "/student",
-            }, {
-              path: "/sdetail",
-            }, {
-              path: "/business",
-            }]
-          }, {
-            path: '/consultant'
-          }, {
-            path: '/playback'
-          }, {
-            path: "/testing",
-            children: [{
-              path: "/testing1",
-            }]
-          },]
-      }, {
-        path: '/living'
-      }]
-      commit(types.SET_MENU, res);
-      resolve(res);
-      // request.get(urls.getMenu).then(res2 => {
-      //   console.log('获取路由', res2)
-      //   commit(types.SET_MENU, res2.data);
-      //   resolve(res2.data);
-      // });
+      // let res = [{
+      //   path: '/index',
+      //   children: [
+      //     {
+      //       path: '/dashboard',
+      //       children: [{
+      //         path: '/schoolDashboard'
+      //       }, {
+      //         path: '/adminDashboard'
+      //       }, {
+      //         path: '/studentDashboard'
+      //       }, {
+      //         path: '/schedules'
+      //       }]
+      //     }, {
+      //       path: '/counseling',
+      //       children: [{
+      //         path: '/baseInfo',
+      //       }, {
+      //         path: '/onlineTest',
+      //       }, {
+      //         path: '/consultLive',
+      //         meta: { step: 4 }
+      //       }, {
+      //         path: '/getReport',
+      //       }]
+      //     }, {
+      //       path: '/knowledge'
+      //     }, {
+      //       path: '/students',
+      //       children: [{
+      //         path: '/studentsList'
+      //       }, {
+      //         path: '/studentsDetail'
+      //       }, {
+      //         path: '/studentsProcess'
+      //       }]
+      //     }, {
+      //       path: '/archives',
+      //       children: [{
+      //         path: '/archivesList'
+      //       }, {
+      //         path: '/archivesDetail'
+      //       }]
+      //     }, {
+      //       path: '/messages'
+      //     }, {
+      //       path: "/personal",
+      //       children: [{
+      //         path: "/studentBase",
+      //       }, {
+      //         path: "/schoolBase",
+      //       }, {
+      //         path: "/consultantBase",
+      //       }, {
+      //         path: "/adminBase",
+      //       }, {
+      //         path: "/account",
+      //       }, {
+      //         path: "/safe",
+      //       }]
+      //     }, {
+      //       path: "/accounts",
+      //       children: [{
+      //         path: "/admin",
+      //       }, {
+      //         path: "/schoolaccount",
+      //       }, {
+      //         path: "/teacher",
+      //       }, {
+      //         path: "/accountlist",
+      //       }, {
+      //         path: "/adminaccountlist",
+      //       }, {
+      //         path: "/schoolaccountlist",
+      //       }, {
+      //         path: "/invitation",
+      //       }]
+      //     }, {
+      //       path: '/live'
+      //     }, {
+      //       path: '/files',
+      //       children: [{
+      //         path: '/filesList'
+      //       }, {
+      //         path: '/filesDetail'
+      //       }]
+      //     },
+      //     {
+      //       path: "/management",
+      //       children: [{
+      //         path: "/counselinglist",
+      //       }, {
+      //         path: "/counselingDetail",
+      //       }, {
+      //         path: "/careerlist",
+      //       }, {
+      //         path: "/careerDetail",
+      //       }, {
+      //         path: "/courselist",
+      //       }, {
+      //         path: "/adviselist",
+      //       }]
+      //     },
+      //     {
+      //       path: "/school",
+      //       children: [{
+      //         path: "/schoollist",
+      //       }, {
+      //         path: "/student",
+      //       }, {
+      //         path: "/sdetail",
+      //       }, {
+      //         path: "/business",
+      //       }]
+      //     }, {
+      //       path: '/consultant'
+      //     }, {
+      //       path: '/playback'
+      //     }, {
+      //       path: "/testing",
+      //       children: [{
+      //         path: "/testing1",
+      //       }]
+      //     },]
+      // }, {
+      //   path: '/living'
+      // }]
+      // commit(types.SET_MENU, res);
+      // resolve(res);
+      request.get(urls.getMenu).then(res2 => {
+        console.log('获取路由', res2)
+        commit(types.SET_MENU, res2.data);
+        resolve(res2.data);
+      });
     });
   },
   setMenu: ({ commit, state }, data) => {
